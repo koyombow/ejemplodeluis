d6cc5e202a035dfd819e69db5353ea3bdffe3481
Alumno <alumno@localhost.localdomain>
2014-08-13 07:38:35 -0600

    mensaje

---
+++ src/luissemana5/semana5.java
@@ -0,0 +1,19 @@
+package luissemana5;
+
+import java.util.Scanner;
+
+public class semana5 {
+
+	public static void main(String[] args) {
+	Scanner s= new Scanner (System.in);
+	String pais;
+	String capital;
+	System.out.print("ingrese el pais:");
+	pais = s.next();
+	System.out.print("capital:");
+	capital = s.next();
+	System.out.print("usted ha ingresado:");
+	System.out.print(pais+capital);   //concatenar es sumar
+	}
+
+}
