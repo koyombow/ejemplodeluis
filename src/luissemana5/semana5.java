c1517e45840b8042d5c50067cd8b2c715649c9ec
Alumno <alumno@localhost.localdomain>
2014-08-13 07:49:22 -0600

    segundo commit

--- src/luissemana5/semana5.java
+++ src/luissemana5/semana5.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class semana5 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {//esto es un cambio
 	Scanner s= new Scanner (System.in);
 	String pais;
 	String capital;
